4907b28b67d9f02bdadae712b4b2b5e6c8531e36
h2echan2 <h2echan2@korea.ac.kr>
2023-01-06 02:10:21 +0900

    Algorithm - STL Practice

--- STL/Pair.cpp
+++ STL/Pair.cpp
@@ -6,14 +6,18 @@ using namespace std;
 int main(int argc, const char * argv[]) {
     pair<int, char> p;
 
-    scanf("%d %c", &p.first, &p.second);
-    printf("%d %c\n", p.first, p.second);
+//    scanf("%d %c", &p.first, &p.second);
+//    printf("%d %c\n", p.first, p.second);
+    cin >> p.first >> p.second;
+    cout << p.first << p.second << endl;
 
     p.first = 1;
     p.second = 'a';
-    printf("%d %c\n", p.first, p.second);
 
+//    printf("%d %c\n", p.first, p.second);
+    cout << p.first << p.second << endl;
     p=make_pair(3, 'b');
-    printf("%d %c\n", p.first, p.second);
 
+//    printf("%d %c\n", p.first, p.second);
+    cout << p.first << p.second << endl;
 }
